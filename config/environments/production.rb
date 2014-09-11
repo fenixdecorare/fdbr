7d45a92bee663d86599ebed47a77b1897a80afef
frugapt <gestao@fruga.pt>
2014-09-12 00:18:12 +0100

    assets compile false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets=true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   # Generate digests for assets URLs.
   config.assets.digest = true
   # `config.assets.compress` has moved to config/initializers/assets.rb
